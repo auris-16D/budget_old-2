0d62fd10e98765bec176d19c3cbb8d144e2e4a11
auris-16D <nigesurtees@gmail.com>
2022-09-15 08:47:29 +0100

    add is_owner check

--- lib/access_control/gate_keeper.rb
+++ lib/access_control/gate_keeper.rb
@@ -1,6 +1,8 @@
 module GateKeeper
 
     def can_access?(user_id, action)
+        return false unless self.is_owner? user_id
+
         policy = ResourcePolicy.find_by(
             resource_name: self.class.name,
             action: [Action.id_from_action(action),Action.id_from_action(Action::FULL)])
@@ -11,11 +13,27 @@ module GateKeeper
             user_id: user_id,
             resource_policy_id: policy.id
         )
-        
-        return user_policy.present?
-      end
 
-      def can_read?(user_id)
+        return user_policy.present?
+    end
+
+    def can_read?(user_id)
         self.can_access?(user_id, Action::READ)
-      end
+    end
+
+    def can_write?(user_id)
+        self.can_access?(user_id, Action::WRITE)
+    end
+
+    def can_update?(user_id)
+        self.can_access?(user_id, Action::UPDATE)
+    end
+
+    def can_delete?(user_id)
+        self.can_access?(user_id, Action::DELETE)
+    end
+
+    def is_owner?(user_id)
+        BudgetUser.find_by(user_id: user_id, budget_space_id: self.budget_space_id).present?
+    end
 end
